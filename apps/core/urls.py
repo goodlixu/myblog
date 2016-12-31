d18d97914ec6567aa308ec53281ddf0e908a04a8
lixu <goodlixu@gmail.com>
2016-12-31 18:04:34 +0800

    debug

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url, include
 from views import about_us, contact
 
 urlpatterns = [
-    url(r'^about_us$', about_us, name='about_us'),
-    url(r'^contact$', contact, name='contact'),
+    url(r'^about_us/$', about_us, name='about_us'),
+    url(r'^contact/$', contact, name='contact'),
 ]
