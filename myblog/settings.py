5bba1d00590d34f22e37d95e4e2495a260bb0f63
lixu <goodlixu@gmail.com>
2016-12-31 21:22:57 +0800

    use mysql

--- myblog/settings.py
+++ myblog/settings.py
@@ -83,9 +83,11 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': '/home/blog/my.cnf',
+        },
     }
 }
 
